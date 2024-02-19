f38f2ae250f3d851070de96255dd1441570fd93f
eFakset <ellen@getacademy.no>
2024-02-19 14:02:01 +0100

    Håndterer feil fra serveren

--- src/pages/ArticleCategories.js
+++ src/pages/ArticleCategories.js
@@ -7,16 +7,31 @@ export default function ArticleCategories() {
     document.title = "Dagligvare - varegrupper";
 
     const [categories, setCategories] = React.useState(null);
+    const [errorMsg, setErrorMsg] = React.useState(null);
+
+    const status = response =>
+    {
+        if (response.status >= 200 && response.status < 300)
+        {
+            return Promise.resolve(response)
+        }
+        return Promise.reject(new Error(response.statusText + ", http error " + response.status))
+    }
 
     React.useEffect(() => {
         fetch("/varegrupper")
-          .then((res) => res.json())
-          .then((data) => setCategories(data.message));
-      }, []);
+            .then(status)
+            .then((res) => res.json())
+            .then((data) => setCategories(data.message))
+            .catch(error => setErrorMsg(error.message))
+        }, []);
 
-    return (
-        <div><h2 align="center">Varegrupper</h2> {!categories ? "Loading..." : <ArticleCategoryTable categories={categories} />}</div>
-    )
+    if (errorMsg)
+        return <h3>Error: {errorMsg}</h3>
+    else
+        return (
+            <div><h2 align="center">Varegrupper</h2> {!categories ? "Loading..." : <ArticleCategoryTable categories={categories} />}</div>
+        )
 }
 
 function ArticleCategoryTable({ categories }) 
